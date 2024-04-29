e7c6ddc8390ccfcc338bea5f6f8d9303f0e209d5
jacobdcook <jacobcookofficial@gmail.com>
2024-04-28 20:09:44 -0700

    For some reason StartGame.java didn't add to the commit, whoops.

--- app/src/main/java/com/gamecodeschool/c17snake/StartGame.java
+++ app/src/main/java/com/gamecodeschool/c17snake/StartGame.java
@@ -5,42 +5,12 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.Log;
-import android.view.SurfaceHolder;
 
 import androidx.core.content.res.ResourcesCompat;
 
 public class StartGame {
-    public static void draw(SurfaceHolder mSurfaceHolder, Canvas mCanvas, int mHighScore, int mScore, Paint mPaint, Apple mApple, Snake mSnake, boolean mPaused, Context mContext) {
-        // Get a lock on the mCanvas
-        if (mSurfaceHolder.getSurface().isValid()) {
-            mCanvas = mSurfaceHolder.lockCanvas();
-
-            // Fill the screen with a color
-            mCanvas.drawColor(Color.parseColor("#f3953d"));
-
-            // Set the size and color of the mPaint for the text
-            mPaint.setColor(Color.parseColor("#000000"));
-
-            mPaint.setTextSize(80);
-
-            // Draw the score
-            mCanvas.drawText("High Score " + mHighScore + " Score:" + mScore, 70, 80, mPaint);
-
-            // Draw the apple and the snake
-            mApple.draw(mCanvas, mPaint);
-            mSnake.draw(mCanvas, mPaint);
-
-            // Draw some text while paused
-            if(mPaused){
-                pauseGame(mPaint,mCanvas,mContext);
-            }
-
-            // Unlock the mCanvas and reveal the graphics for this frame
-            mSurfaceHolder.unlockCanvasAndPost(mCanvas);
-        }
-    }
-    public static void pauseGame(Paint mPaint, Canvas mCanvas, Context mContext){
-        Log.d("tag223","paused");
+    public static void pauseGame(Paint mPaint, Canvas mCanvas, Context mContext) {
+        Log.d("tag223", "paused");
 
         // Set the size and color of the mPaint for the text
         mPaint.setColor(Color.parseColor("#000000"));
@@ -48,10 +18,6 @@ public class StartGame {
         mPaint.setTypeface(ResourcesCompat.getFont(mContext, R.font.poppins_regular));
 
         // Draw the message
-        // We will give this an international upgrade soon
-        //mCanvas.drawText("Tap To Play!", 200, 700, mPaint);
-        mCanvas.drawText("Tap to Play",
-                200, 200, mPaint);
+        mCanvas.drawText("Tap to Play", 200, 200, mPaint);
     }
-
 }
