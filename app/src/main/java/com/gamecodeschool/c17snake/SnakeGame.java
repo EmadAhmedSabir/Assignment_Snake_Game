a2bc832f05895ac7061e62e8f6361384173d3ae6
pprachi15 <pprachi1510@gmail.com>
2024-05-12 17:53:02 -0700

    Revert ""added sound for game over and game's background music""
    This reverts commit cc9901cd0b1ca7e176b6ca48b3ff6c6d72b3fa3f.

--- app/src/main/java/com/gamecodeschool/c17snake/SnakeGame.java
+++ app/src/main/java/com/gamecodeschool/c17snake/SnakeGame.java
@@ -114,7 +114,7 @@ public class SnakeGame extends SurfaceView implements Runnable {
             descriptor = assetManager.openFd("get_apple.ogg");
             mEat_ID = mSP.load(descriptor, 0);
 
-            descriptor = assetManager.openFd("game_over.ogg");
+            descriptor = assetManager.openFd("snake_death.ogg");
             mCrashID = mSP.load(descriptor, 0);
 
         } catch (IOException e) {
@@ -299,7 +299,7 @@ public class SnakeGame extends SurfaceView implements Runnable {
     }
     private void updateScoreDisplay() {
         mTxtScore.post(() -> mTxtScore.setText("Score: " + mScore));
-        mTxtHighScore.post(() -> mTxtHighScore.setText("High Score: " + mHighScore));
+        mTxtHighScore.post(() -> mTxtHighScore.setText("HiScore: " + mHighScore));
     }
 
 
