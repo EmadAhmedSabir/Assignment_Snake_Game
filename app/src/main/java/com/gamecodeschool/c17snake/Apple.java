52081b6d414118965d30b879729cb4e77aba371c
jacobdcook <jacobcookofficial@gmail.com>
2024-05-13 19:04:52 -0700

    Implemented Decorator pattern in Apple.java to allow for dynamic decoration of Apple objects.

--- app/src/main/java/com/gamecodeschool/c17snake/Apple.java
+++ app/src/main/java/com/gamecodeschool/c17snake/Apple.java
@@ -13,67 +13,104 @@ import android.graphics.PorterDuffColorFilter;
 @SuppressWarnings("all")
 
 class Apple implements Drawable {
-    private static Apple instance = null;
-
-    private final Point location = new Point();
-    private final Point mSpawnRange;
-    private int mSize;
-
-    private Bitmap mOriginalBitmap;
-    private Bitmap mBitmapApple;
-
-    private final Context context;
-    private static final int initialX = -10;
+    private AppleDecorator appleDecorator;
 
     private Apple(Context context, Point sr, int s) {
-        this.context = context;
-        mSpawnRange = sr;
-        mSize = s;
-        location.x = initialX;
-
-        mOriginalBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.apple);
-        mBitmapApple = Bitmap.createScaledBitmap(mOriginalBitmap, s, s, false);
+        // Initialize the basic Apple object
+        appleDecorator = new BasicApple(context, sr, s);
     }
 
-    public static synchronized Apple getInstance(Context context, Point sr, int s) {
-        if (instance == null) {
-            instance = new Apple(context, sr, s);
-        }
-        return instance;
+    public static Apple getInstance(Context context, Point sr, int s) {
+        return new Apple(context, sr, s);
     }
 
     void spawn() {
-        Random random = new Random();
-        location.x = random.nextInt(mSpawnRange.x - 1) + 1;
-        location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+        appleDecorator.spawn();
     }
 
     void spawn(int s) {
-        Random random = new Random();
-        location.x = random.nextInt(mSpawnRange.x - 1) + 1;
-        location.y = random.nextInt(mSpawnRange.y - 1) + 1;
-        mBitmapApple = Bitmap.createScaledBitmap(mOriginalBitmap, mSize, mSize, false);
-        changeColor(s);
+        appleDecorator.spawn(s);
     }
 
     void changeColor(int s) {
-        Bitmap tempBitmap = Bitmap.createBitmap(mSize, mSize, Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(tempBitmap);
-        Paint paint = new Paint();
-        int newColor = s;
-        PorterDuffColorFilter colorFilter = new PorterDuffColorFilter(newColor, PorterDuff.Mode.SRC_ATOP);
-        paint.setColorFilter(colorFilter);
-
-        canvas.drawBitmap(mBitmapApple, 0, 0, paint);
-        mBitmapApple = tempBitmap;
+        appleDecorator.changeColor(s);
     }
 
     Point getLocation() {
-        return location;
+        return appleDecorator.getLocation();
     }
 
     @Override
     public void draw(Canvas canvas, Paint paint) {
-        canvas.drawBitmap(mBitmapApple, location.x * mSize, location.y * mSize, paint);
+        appleDecorator.draw(canvas, paint);
+    }
+
+    // Decorator interface
+    private interface AppleDecorator extends Drawable {
+        void spawn();
+        void spawn(int s);
+        void changeColor(int s);
+        Point getLocation();
+    }
+
+    private class BasicApple implements AppleDecorator {
+        private final Point location = new Point();
+        private final Point mSpawnRange;
+        private int mSize;
+
+        private Bitmap mOriginalBitmap;
+        private Bitmap mBitmapApple;
+
+        private final Context context;
+        private static final int initialX = -10;
+
+        private BasicApple(Context context, Point sr, int s) {
+            this.context = context;
+            mSpawnRange = sr;
+            mSize = s;
+            location.x = initialX;
+
+            mOriginalBitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.apple);
+            mBitmapApple = Bitmap.createScaledBitmap(mOriginalBitmap, s, s, false);
+        }
+
+        @Override
+        public void spawn() {
+            Random random = new Random();
+            location.x = random.nextInt(mSpawnRange.x - 1) + 1;
+            location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+        }
+
+        @Override
+        public void spawn(int s) {
+            Random random = new Random();
+            location.x = random.nextInt(mSpawnRange.x - 1) + 1;
+            location.y = random.nextInt(mSpawnRange.y - 1) + 1;
+            mBitmapApple = Bitmap.createScaledBitmap(mOriginalBitmap, mSize, mSize, false);
+            changeColor(s);
+        }
+
+        @Override
+        public void changeColor(int s) {
+            Bitmap tempBitmap = Bitmap.createBitmap(mSize, mSize, Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(tempBitmap);
+            Paint paint = new Paint();
+            int newColor = s;
+            PorterDuffColorFilter colorFilter = new PorterDuffColorFilter(newColor, PorterDuff.Mode.SRC_ATOP);
+            paint.setColorFilter(colorFilter);
+
+            canvas.drawBitmap(mBitmapApple, 0, 0, paint);
+            mBitmapApple = tempBitmap;
+        }
+
+        @Override
+        public Point getLocation() {
+            return location;
+        }
+
+        @Override
+        public void draw(Canvas canvas, Paint paint) {
+            canvas.drawBitmap(mBitmapApple, location.x * mSize, location.y * mSize, paint);
+        }
     }
 }
